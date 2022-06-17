30a409b4b3d9c8d6b9d529264e5d6c69ccd82ec4
lieinsay <1535736017@qq.com>
2022-06-17 15:19:58 +0800

    update Source

---
+++ Source/VRImmersiveHands/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class VRIMMERSIVEHANDS_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
